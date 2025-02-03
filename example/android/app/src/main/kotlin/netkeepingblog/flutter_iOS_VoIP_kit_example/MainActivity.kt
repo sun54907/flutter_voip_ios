ffddd05b576e9924d7205640f7a06e3d521fe5dd
umikawa kazuya <umikawa@calldoctor.co.jp>
2025-02-03 18:28:10 +0900

    :wrench: update example

--- example/android/app/src/main/kotlin/netkeepingblog/flutter_iOS_VoIP_kit_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/netkeepingblog/flutter_iOS_VoIP_kit_example/MainActivity.kt
@@ -2,5 +2,4 @@ package netkeepingblog.flutter_ios_voip_kit_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
